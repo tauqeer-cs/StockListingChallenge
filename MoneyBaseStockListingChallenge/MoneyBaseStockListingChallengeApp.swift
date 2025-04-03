f6ef629b148d3b4212e2da4b6565844351060add
Tauqeer-Mindvalley <164833402+Tauqeer-Mindvalley@users.noreply.github.com>
2025-04-03 11:36:51 +0700

    Initial Commit

---
+++ MoneyBaseStockListingChallenge/MoneyBaseStockListingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyBaseStockListingChallengeApp.swift
+//  MoneyBaseStockListingChallenge
+//
+//  Created by Tauqeer on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyBaseStockListingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
